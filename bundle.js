a3b433c325889c6e8775f4165c10bb4d3032cef8
Christie Corcoran <christiecorcoran30@gmail.com>
2021-07-16 11:18:19 -0400

    Updates

---
+++ bundle.js
@@ -0,0 +1,238 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./js/app.js":
+/*!*******************!*\
+  !*** ./js/app.js ***!
+  \*******************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _welcome_html__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../welcome.html */ \"./welcome.html\");\n/* harmony import */ var _experience_html__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../experience.html */ \"./experience.html\");\n/* harmony import */ var _portfolio_html__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../portfolio.html */ \"./portfolio.html\");\n/* harmony import */ var _img_CC_png__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../img/CC.png */ \"./img/CC.png\");\n/* harmony import */ var _img_christie_jpg__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../img/christie.jpg */ \"./img/christie.jpg\");\n/* harmony import */ var _img_mountains_jpg__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../img/mountains.jpg */ \"./img/mountains.jpg\");\n/* harmony import */ var _img_sas_png__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../img/sas.png */ \"./img/sas.png\");\n/* harmony import */ var _img_saseg_png__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../img/saseg.png */ \"./img/saseg.png\");\n/* harmony import */ var _img_sasstudio_png__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ../img/sasstudio.png */ \"./img/sasstudio.png\");\n// CSS\r\n__webpack_require__(/*! ../css/styles.css */ \"./css/styles.css\");\r\n\r\n// HTML\r\n\r\n\r\n\r\n\r\n// Images\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n// JavaScript\r\n__webpack_require__(/*! ./main.js */ \"./js/main.js\");\n\n//# sourceURL=webpack://Personal-Website/./js/app.js?");
+
+/***/ }),
+
+/***/ "./js/main.js":
+/*!********************!*\
+  !*** ./js/main.js ***!
+  \********************/
+/***/ (() => {
+
+eval("\r\n\r\n$(document).ready(function() {\r\n \r\n\r\n});\r\n\r\n\n\n//# sourceURL=webpack://Personal-Website/./js/main.js?");
+
+/***/ }),
+
+/***/ "./img/CC.png":
+/*!********************!*\
+  !*** ./img/CC.png ***!
+  \********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"img/CC.png\");\n\n//# sourceURL=webpack://Personal-Website/./img/CC.png?");
+
+/***/ }),
+
+/***/ "./img/christie.jpg":
+/*!**************************!*\
+  !*** ./img/christie.jpg ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"img/christie.jpg\");\n\n//# sourceURL=webpack://Personal-Website/./img/christie.jpg?");
+
+/***/ }),
+
+/***/ "./img/mountains.jpg":
+/*!***************************!*\
+  !*** ./img/mountains.jpg ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"img/mountains.jpg\");\n\n//# sourceURL=webpack://Personal-Website/./img/mountains.jpg?");
+
+/***/ }),
+
+/***/ "./img/sas.png":
+/*!*********************!*\
+  !*** ./img/sas.png ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"img/sas.png\");\n\n//# sourceURL=webpack://Personal-Website/./img/sas.png?");
+
+/***/ }),
+
+/***/ "./img/saseg.png":
+/*!***********************!*\
+  !*** ./img/saseg.png ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"img/saseg.png\");\n\n//# sourceURL=webpack://Personal-Website/./img/saseg.png?");
+
+/***/ }),
+
+/***/ "./img/sasstudio.png":
+/*!***************************!*\
+  !*** ./img/sasstudio.png ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"img/sasstudio.png\");\n\n//# sourceURL=webpack://Personal-Website/./img/sasstudio.png?");
+
+/***/ }),
+
+/***/ "./welcome.html":
+/*!**********************!*\
+  !*** ./welcome.html ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"welcome.html\");\n\n//# sourceURL=webpack://Personal-Website/./welcome.html?");
+
+/***/ }),
+
+/***/ "./experience.html":
+/*!*************************!*\
+  !*** ./experience.html ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"experience.html\");\n\n//# sourceURL=webpack://Personal-Website/./experience.html?");
+
+/***/ }),
+
+/***/ "./portfolio.html":
+/*!************************!*\
+  !*** ./portfolio.html ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"portfolio.html\");\n\n//# sourceURL=webpack://Personal-Website/./portfolio.html?");
+
+/***/ }),
+
+/***/ "./css/styles.css":
+/*!************************!*\
+  !*** ./css/styles.css ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://Personal-Website/./css/styles.css?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./js/app.js");
+/******/ 	
+/******/ })()
+;
